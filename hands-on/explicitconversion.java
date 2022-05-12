32916cfdf03da7f68c3b107021c2ab10fa21f795
Amruta118 <96807863+Amruta118@users.noreply.github.com>
2022-05-12 13:06:14 +0530

    Add files via upload

---
+++ hands-on/explicitconversion.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class explicitconversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//converting double to integer
+		double d = 200.06;
+	
+		int i = (int)d;
+		System.out.println("Integer Data type value "+i);
+
+	}
+
+}
