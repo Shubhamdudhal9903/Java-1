1a242fb398084d558d3e604720478461c6d53f5a
Amruta118 <96807863+Amruta118@users.noreply.github.com>
2022-05-19 11:53:50 +0530

    Add files via upload

---
+++ Practical/Defaultconstructor.java
@@ -0,0 +1,15 @@
+package corejava;
+
+class Defaultconstructor {
+	Defaultconstructor()
+	{
+		System.out.println("This is Default Contructor OR No argument constructor");
+	}
+
+	public static void main(String[] args) {
+		Defaultconstructor m = new Defaultconstructor();
+		
+
+	}
+
+}
